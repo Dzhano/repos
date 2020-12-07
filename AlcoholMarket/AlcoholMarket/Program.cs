413623be0aa0a8b14bac58d7cd7f930c9dff65ef
Dzhano <Djano@students.softuni.bg>
2020-12-07 08:59:58 +0200

    Another test / Друг тест

--- AlcoholMarket/AlcoholMarket/Program.cs
+++ AlcoholMarket/AlcoholMarket/Program.cs
@@ -6,13 +6,13 @@ namespace AlcoholMarket
     {
         static void Main(string[] args)
         {
-            double whiskeyForLiter = double.Parse(Console.ReadLine());
+            double whiskeiyfkffliyForLiter = double.Parse(Console.ReadLine());
             double amountBeer = double.Parse(Console.ReadLine());
             double amountWine = double.Parse(Console.ReadLine());
             double amountRaki = double.Parse(Console.ReadLine());
             double amountWhiskey = double.Parse(Console.ReadLine());
 
-            double rakiForLiter = whiskeyForLiter / 2;
+            double rakiForLiter = whiskeiyfkffliyForLiter / 2;
             double wineForLiter = rakiForLiter - (0.4 * rakiForLiter);
             double beerForLiter = rakiForLiter - (0.8 * rakiForLiter);
 
